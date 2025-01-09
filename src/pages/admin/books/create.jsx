129b20437289dd4fb634ba94263b888e0b5a3c02
hiphipdora <faaay.adistiii@gmail.com>
2025-01-09 13:57:55 +0700

    remove: admin dark template

--- src/pages/admin/books/create.jsx
+++ src/pages/admin/books/create.jsx
@@ -3,12 +3,12 @@ export default function BookCreate() {
   return (
     <div className="flex flex-col gap-9">
       <div
-        className="rounded-sm bg-white shadow-default dark:bg-boxdark"
+        className="rounded-sm bg-white shadow-default :bg-box"
       >
         <div
-          className="border-b border-stroke px-6.5 py-4 dark:border-strokedark"
+          className="border-b border-stroke px-6.5 py-4 :border-stroke"
         >
-          <h3 className="font-medium text-black dark:text-white">
+          <h3 className="font-medium text-black :text-white">
             Add Data
           </h3>
         </div>
@@ -17,79 +17,79 @@ export default function BookCreate() {
 
             <div className="mb-4.5">
               <label
-                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                className="mb-3 block text-sm font-medium text-black :text-white"
               >
                 Title
               </label>
               <input
                 type="text"
-                className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-indigo-600"
+                className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter :border-form-stroke :bg-form-input :text-white :focus:border-indigo-600"
               />
             </div>
 
             <div className="mb-4.5">
               <label
-                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                className="mb-3 block text-sm font-medium text-black :text-white"
               >
                 Description
               </label>
               <textarea
                 rows="6"
-                className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-indigo-600"
+                className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter :border-form-stroke :bg-form-input :text-white :focus:border-indigo-600"
               ></textarea>
             </div>
 
             <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
               <div className="w-full xl:w-1/2">
                 <label
-                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  className="mb-3 block text-sm font-medium text-black :text-white"
                 >
                   Price
                 </label>
                 <input
                   type="number"
                   min={1}
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-indigo-600"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter :border-form-stroke :bg-form-input :text-white :focus:border-indigo-600"
                 />
               </div>
 
               <div className="w-full xl:w-1/2">
                 <label
-                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  className="mb-3 block text-sm font-medium text-black :text-white"
                 >
                   Stock
                 </label>
                 <input
                   type="number"
                   min={1}
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-indigo-600"
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter :border-form-stroke :bg-form-input :text-white :focus:border-indigo-600"
                 />
               </div>
             </div>
 
             <div className="mb-4.5">
               <label
-                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                className="mb-3 block text-sm font-medium text-black :text-white"
               >
                 Attach file
               </label>
               <input
                 type="file"
-                className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-indigo-600 file:hover:bg-opacity-10 focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-indigo-600"
+                className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-indigo-600 file:hover:bg-opacity-10 focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter :border-form-stroke :bg-form-input :file:border-form-stroke :file:bg-white/30 :file:text-white :focus:border-indigo-600"
               />
             </div>
 
             <div className="mb-4.5">
               <label
-                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                className="mb-3 block text-sm font-medium text-black :text-white"
               >
                 Genre
               </label>
               <div
-                className="relative z-20 bg-transparent dark:bg-form-input"
+                className="relative z-20 bg-transparent :bg-form-input"
               >
                 <select
-                  className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-indigo-600 active:border-indigo-600 dark:border-form-strokedark dark:bg-form-input dark:focus:border-indigo-600"
+                  className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-indigo-600 active:border-indigo-600 :border-form-stroke :bg-form-input :focus:border-indigo-600"
                 >
                   <option value="" className="text-body">
                     --select genre--
@@ -123,15 +123,15 @@ export default function BookCreate() {
             </div>
             <div className="mb-4.5">
               <label
-                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                className="mb-3 block text-sm font-medium text-black :text-white"
               >
                 Author
               </label>
               <div
-                className="relative z-20 bg-transparent dark:bg-form-input"
+                className="relative z-20 bg-transparent :bg-form-input"
               >
                 <select
-                  className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-indigo-600 active:border-indigo-600 dark:border-form-strokedark dark:bg-form-input dark:focus:border-indigo-600"
+                  className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-indigo-600 active:border-indigo-600 :border-form-stroke :bg-form-input :focus:border-indigo-600"
                 >
                   <option value="" className="text-body">
                     --select author--
--- src/pages/admin/books/index.jsx
+++ src/pages/admin/books/index.jsx
@@ -7,24 +7,24 @@ export default function AdminBooks() {
     >
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
-          <thead className="border-b bg-gray-50 text-white">
+          <thead className="border-b bg-gray-50 text-gray-900">
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
               <th
-                className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11"
+                className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-gray-900 xl:pl-11"
               >
                 Package
               </th>
               <th
-                className="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white"
+                className="min-w-[150px] px-4 py-4 font-medium text-black dark:text-gray-900"
               >
                 Invoice date
               </th>
               <th
-                className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white"
+                className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-gray-900"
               >
                 Status
               </th>
-              <th className="px-4 py-4 font-medium text-black dark:text-white">
+              <th className="px-4 py-4 font-medium text-black dark:text-gray-900">
                 Actions
               </th>
             </tr>
@@ -35,11 +35,11 @@ export default function AdminBooks() {
               <td
                 className="px-4 py-5 pl-9 xl:pl-11"
               >
-                <h5 className="font-medium text-black dark:text-white">Free Package</h5>
+                <h5 className="font-medium text-black dark:text-gray-900">Free Package</h5>
                 <p className="text-sm">$0.00</p>
               </td>
               <td className="px-4 py-5">
-                <p className="text-black dark:text-white">Jan 13,2023</p>
+                <p className="text-black dark:text-gray-900">Jan 13,2023</p>
               </td>
               <td className="px-4 py-5">
                 <p
@@ -66,13 +66,13 @@ export default function AdminBooks() {
               <td
                 className="px-4 py-5 pl-9 xl:pl-11"
               >
-                <h5 className="font-medium text-black dark:text-white">
+                <h5 className="font-medium text-black dark:text-gray-900">
                   Standard Package
                 </h5>
                 <p className="text-sm">$59.00</p>
               </td>
               <td className="px-4 py-5">
-                <p className="text-black dark:text-white">Jan 13,2023</p>
+                <p className="text-black dark:text-gray-900">Jan 13,2023</p>
               </td>
               <td className="px-4 py-5">
                 <p
